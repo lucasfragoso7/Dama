983f492784f676734e6f0d11482cab6e85a72d40
andre fragoso <andre.moreira@ctis.com.br>
2019-11-26 00:45:59 -0300

    Metodo para obter o nome do primeiro e do segundo jogador

--- src/dama/Main.java
+++ src/dama/Main.java
@@ -7,22 +7,24 @@ package dama;
 
 import java.io.IOException;
 
+import javax.swing.JOptionPane;
+
 import dao.GravadorDosJogos;
 import interfaceGrafica.Janela;
 import interfaceGrafica.PlayerController;
-
 
 public class Main {
 
 	private static Tabuleiro t;
 
-	public static void empate(){
+	public static void empate() {
 		t.declaraVencedor(true);
 	}
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) throws IOException, ClassNotFoundException {
+
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) throws IOException, ClassNotFoundException {
 
 //		ObjectInputStream oin = new ObjectInputStream(new FileInputStream("pesos.object"));
 //		double x[] = (double[]) oin.readObject();
@@ -31,25 +33,22 @@ public class Main {
 //		}
 //		oin.close();
 
-
 		Janela j = new Janela();
-
+		j.setTitle(j.getLabelOne() + " vs " + j.getLabelTwo());
 		t = new Tabuleiro();
 		Jogador v = new Jogador(t, true);
 		Jogador p = new Jogador(t, false);
 		PlayerController pc = new PlayerController(p, v);
 		j.setPlayerInterface(pc);
-		
+
 		GravadorDosJogos logger = new GravadorDosJogos();
-	
 
 		j.getGUI().setPlayerInterface(pc);
 		j.setTabuleiro(t);
 		j.setVisible(true);
-		
+
+	}
 
 
-		
-    }
 
 }
